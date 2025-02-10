4e25d0e20a7d3dfbf0d986e7ff3e9874e8908235
eihigh <eihigh.contact@gmail.com>
2025-02-11 01:32:35 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/eihigh/audio-issue-3192
+
+go 1.23.4
+
+require github.com/hajimehoshi/ebiten/v2 v2.8.6
+
+require (
+	github.com/ebitengine/oto/v3 v3.3.2 // indirect
+	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
+	github.com/jfreymuth/oggvorbis v1.0.5 // indirect
+	github.com/jfreymuth/vorbis v1.0.2 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bytes"
+	"embed"
+	"flag"
+	"fmt"
+	"io"
+	"strings"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/audio/mp3"
+	"github.com/hajimehoshi/ebiten/v2/audio/vorbis"
+)
+
+var (
+	file     = flag.String("file", "cat.ogg", "path to play")
+	typ      = flag.String("type", "Reader", "type of audio source (Reader or ReadSeeker)")
+	panics   = flag.Bool("panics", false, "panics on eof")
+	context  = flag.Int("context", 48000, "sampleRate of audio context")
+	resample = flag.Int("resample", 48000, "sampleRate of resampling")
+	listen   = flag.Int("listen", 5, "listen time in seconds")
+)
+
+//go:embed *.ogg *.mp3
+var fsys embed.FS
+
+type reader struct {
+	src io.ReadSeeker
+}
+
+func (r *reader) Read(p []byte) (n int, err error) {
+	n, err = r.src.Read(p)
+	if *panics && err == io.EOF {
+		panic("eof")
+	}
+	return n, err
+}
+
+type readSeeker struct {
+	src io.ReadSeeker
+}
+
+func (r *readSeeker) Read(p []byte) (n int, err error) {
+	n, err = r.src.Read(p)
+	if *panics && err == io.EOF {
+		panic("eof")
+	}
+	return n, err
+}
+
+func (r *readSeeker) Seek(offset int64, whence int) (int64, error) {
+	return r.src.Seek(offset, whence)
+}
+
+func run() error {
+	flag.Parse()
+
+	b, err := fsys.ReadFile(*file)
+	if err != nil {
+		return err
+	}
+
+	var r io.Reader
+	switch *typ {
+	case "Reader":
+		r = &reader{src: bytes.NewReader(b)}
+	case "ReadSeeker":
+		r = &readSeeker{src: bytes.NewReader(b)}
+	default:
+		panic("invalid -type")
+	}
+
+	ctx := audio.NewContext(*context)
+
+	var p *audio.Player
+	switch {
+	case strings.HasSuffix(*file, ".ogg"):
+		stream, err := vorbis.DecodeWithSampleRate(*resample, r)
+		if err != nil {
+			return err
+		}
+		fmt.Println(stream.Length())
+		p, err = ctx.NewPlayer(stream)
+		if err != nil {
+			return err
+		}
+
+	case strings.HasSuffix(*file, ".mp3"):
+		stream, err := mp3.DecodeWithSampleRate(*resample, r)
+		if err != nil {
+			return err
+		}
+		fmt.Println(stream.Length())
+		p, err = ctx.NewPlayer(stream)
+		if err != nil {
+			return err
+		}
+
+	default:
+		panic("invalid file format")
+	}
+
+	p.SetVolume(0.3)
+	p.Play()
+
+	<-time.After(time.Duration(*listen) * time.Second)
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
